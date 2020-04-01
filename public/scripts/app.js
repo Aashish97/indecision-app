65df8ddb540985710952d7d0f42dd546ff60941e
aashish <aashish@ubuntu>
2020-04-01 11:23:26 +0545

    Changes by babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,48 @@
-"use strict";
+'use strict';
 
 console.log("App is running");
 
 var appRoot = document.getElementById('app');
 
-var template = React.createElement(
-  "h1",
-  null,
-  "Indecision Apps"
-);
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in hands of computer',
+    options: ['One', 'Two']
+};
 
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
 ReactDOM.render(template, appRoot);
+
+renderCountTemplate();
