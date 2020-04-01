284d2405b643d0afd29ced5f94c9ed4f8600e8ca
aashish <aashish@ubuntu>
2020-04-01 13:24:29 +0545

    Added array features in app

--- src/app.js
+++ src/app.js
@@ -5,20 +5,45 @@ const appRoot = document.getElementById('app');
 const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in hands of computer',
-    options: ['One', 'Two']
+    options: []
 }
 
-const template = (
-    <div>
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value;
+    if (option){
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    }
+    renderTemplate();
+};
+
+const onRemoveAll = () => {
+    app.options = [];
+    renderTemplate();
+}
+
+const renderTemplate = () => {
+    const template = (
+        <div>
         <h1>{app.title}</h1>
         <p>{app.subtitle}</p>
         <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
-ReactDOM.render(template, appRoot);
+        <p>{app.options.length}</p>
+        <button onClick={onRemoveAll}>Remove All</button>
+            <ol>
+                {
+                    app.options.map(option => <li key={option}>{option}</li>)
+                }
+            </ol>
+    
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add Option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+}
 
-renderCountTemplate();
+renderTemplate();
