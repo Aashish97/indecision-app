4dad64e1810227edc264d244caf5d8fe5eca0286
aashish <aashish@ubuntu>
2020-04-04 16:06:17 +0545

    Refactored code

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import IndecisionApp from "./components/IndecisionApp";
 
-const template = <p>From JSX</p>
 
-ReactDom.render(template, document.getElementById('app'));
+
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
