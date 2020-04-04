2feb7d8ca44462943c83103b391b9ebf0be14d51
aashish <aashish@ubuntu>
2020-04-04 23:21:07 +0545

    Created environment for the scss styling

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -82,17 +82,22 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
-        <Action
-          hasOption={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          hasOption={this.state.options.length > 0}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption handleAddOption={this.handleAddOption} />
+
+        <div className="container">
+          <Action
+            hasOption={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              hasOption={this.state.options.length > 0}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption handleAddOption={this.handleAddOption} />
+          </div>
+        </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
--- src/components/Option.js
+++ src/components/Option.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const Option = props => {
   return (
-    <div>
-      {props.optionText}
-      <button onClick={e => props.handleDeleteOption(props.optionText)}>
+    <div className="option">
+      <p className="option__text">{props.count}. {props.optionText}</p>
+      <button
+        onClick={e => props.handleDeleteOption(props.optionText)}
+        className="button button--link"
+      >
         Remove
       </button>
     </div>
